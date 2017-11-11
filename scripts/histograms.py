2001a85b8f94762526ba252d9748067588e5c2f3
Dawid Wiśniewski <dawid.wisniewski2013@outlook.com>
2017-11-11 19:46:32 +0100

    Histogram liczby wyświetleń

--- scripts/histograms.py
+++ scripts/histograms.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 # Histogramy (łącznie z wykresem) liczby wyświetleń, odpowiedzi oraz sprzedaży (*),
 
 import pandas
 import os
+import numpy as np
 import matplotlib.pyplot as plot
 from collections import defaultdict
 
-#["id", 
+# ["id",
 # "region_id", 
 # "category_id", 
 # "subregion_id", 
@@ -34,7 +36,7 @@ from collections import defaultdict
 # "reply_chat_intent"]
 
 data_dir = os.path.dirname(os.path.abspath(__file__)) + "\..\data\\ads"
-col_used = [0,21,22,23]
+col_used = [0, 21, 22, 23]
 
 dirs = []
 for path, subdirs, files in os.walk(data_dir):
@@ -42,9 +44,33 @@ for path, subdirs, files in os.walk(data_dir):
         dirs.append(os.path.join(path, name))
 
 daily_ads = []
+dataFrame = []
 for file_index, file_name in enumerate(dirs):
-    if file_index < 3:
+    if file_index < 11:
         if ".git" not in file_name:
-            daily_ads.append(pandas.read_csv(file_name, header = 0, usecols = col_used))
+            #daily_ads.append(pandas.read_csv(file_name, header=0, usecols=col_used, nrows=5))
+            daily_ads.append(pandas.read_csv(file_name, header=0, usecols=col_used))
 
-print(daily_ads)
+#print(daily_ads)
+
+#print "\nconcated\n"
+dataFrame = pandas.concat(daily_ads)
+#print dataFrame
+
+#print "\ngroupby\n"
+groupedDF = dataFrame.groupby(['id']).sum()
+#print groupedDF
+
+maxValue = groupedDF["predict_views"].max()
+step = 500
+#groupedDF = groupedDF.groupby(pandas.cut(groupedDF["predict_views"], np.arange(0, maxValue+step, step))).count()
+
+#print "\ncut\n"
+#print maxValue
+print groupedDF
+
+plot.hist(dataFrame.groupby(['id']).sum()["predict_views"], range(0, maxValue+step, step), log=True)
+plot.xlabel('Views')
+plot.ylabel('Ads')
+plot.grid(True)
+plot.show()
